201f91420529ec1a8a69685345415f551db381fa
George Mayer <mayer.georgep@gmail.com>
2025-03-24 15:26:29 -0500

    first commit

---
+++ src/treebeard/__init__.py
@@ -0,0 +1,11 @@
+"""
+Treebeard - A Python library for forwarding logs to endpoints
+"""
+
+from .core import Treebeard
+from .context import LoggingContext
+from .log import Log
+
+__version__ = "0.1.0"
+
+__all__ = ["Treebeard", "LoggingContext", "Log"]
