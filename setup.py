9369d7f33352e24fbeb8d62c9cb735ef5e02e557
George Mayer <mayer.georgep@gmail.com>
2025-05-09 17:15:38 -0500

    0.4.10.dev0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = "treebeardhq"
 
 setup(
     name=package_name,
-    version="0.4.9dev5",
+    version="0.4.10dev0",
     packages=find_packages(include=[package_name, f"{package_name}.*"]),
     description="Treebeard logging library",
     author="George Mayer",
